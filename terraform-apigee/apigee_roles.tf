76a57767bb3397e5cc813edd6a2cdc7b30db4380
danielitogomez <daniel.gomez.amaran@gmail.com>
2021-05-31 13:23:24 -0400

    add new configs

--- terraform-apigee/apigee_roles.tf
+++ terraform-apigee/apigee_roles.tf
@@ -3,19 +3,6 @@ resource "apigee_role" "Developer" {
   name = "Developer"
 }
 
-variable "permissions_Developer" {
-  default = [
-    "/applications",
-    "/apps",
-    "/apiproducts",
-    "/reports",
-    "/deployments",
-    "/environments/*/keystores",
-    "/keyvaluemaps",
-    "/environments/*/deployments"
-  ]
-}
-
 resource "apigee_role_permission" "Developer" {
   role_name = apigee_role.Developer.name
   count     = length(var.permissions_Developer)
--- terraform-apigee/variables.tf
+++ terraform-apigee/variables.tf
@@ -1,3 +1,8 @@
+# Organization
+variable "organization" {
+  default = "danielgomezamaran-eval"
+}
+
 variable "username" {
   default = ""
 }
@@ -6,8 +11,20 @@ variable "password" {
   default = ""
 }
 
-variable "organization" {
-  default = ""
+# Developer
+variable "permissions_Developer" {
+  default = [
+    "/applications",
+    "/apps",
+    "/apiproducts",
+    "/reports",
+    "/deployments",
+    "/environments/*/keystores",
+    "/keyvaluemaps",
+    "/environments/*/deployments"
+  ]
 }
 
-
+variable "mails_Developer" {
+  default = ["daniel.gomez.a@usach.cl"]
+}
